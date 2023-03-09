3d76ccb6c35cccda071f00ffc662461010bd596c
Bright <108338051+I3right@users.noreply.github.com>
2023-03-09 19:05:29 +0700

    update evening

---
+++ ClassJSD/JS/js101/script.js
@@ -0,0 +1,12 @@
+function showalert() {
+  alert("Helloworld");
+  console.log("button has been clicked");
+  document.getElementById("printworld").innerText = "buttons has been clicked";
+}
+
+/* 
+Variable 
+3 type : 
+var = global scope
+let const = block scope work on that block
+*/
